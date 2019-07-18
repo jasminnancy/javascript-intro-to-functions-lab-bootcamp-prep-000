8e8327800d587b98dbf14c5a5bf8ab43bff81c7e
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-07-18 16:43:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function sayHiToGrandma(string) {
   var mixedCase = "Hi there!"
   
   if (string.toLowerCase() === lowercase {
-    return "I can\'t hear you!"}
-    else if (string.toUpperCase() === uppercase {return "YES INDEED!"})
+    return "I can\'t hear you!"
+  }
+  else if (string.toUpperCase() === uppercase {return "YES INDEED!"})
       else if (string === "I love you, Grandma.") {return "I love you, too."}
 }
