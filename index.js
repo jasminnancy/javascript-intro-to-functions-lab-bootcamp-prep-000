ef54dfbb2bdcf84934c9585120c35cb7761cf8dc
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-07-18 16:07:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 function shout(string) { return string.toUpperCase()}
 function whisper(string) {return string.toLowerCase()}
-var consoleLog = console.log()
-function logShout(string) {consoleLog}
+function logShout(string) {console.log(string.toUpperCase())}
