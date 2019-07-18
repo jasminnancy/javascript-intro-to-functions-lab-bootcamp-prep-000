f8912e69cfcdfe2ea19aa97b65d48ac4e03a5c86
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-07-18 16:56:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function whisper(string) {return string.toLowerCase()}
 function logShout(string) {console.log(string.toUpperCase())}
 function logWhisper(string) {console.log(string.toLowerCase())}
 function sayHiToGrandma(string) {
-  var uppercase = "HELLO!"
+  var uppercase = "HELLO"
   var lowercase = "hello!"
   var mixedcase = "I love you, Grandma."
   
